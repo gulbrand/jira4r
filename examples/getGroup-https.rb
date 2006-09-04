3beb577dd64eae0122acd37f7bc6d5691425c1f2
bwalding <bwalding@eca90e12-3019-0410-aa8e-b962190924c8>
2006-09-04 10:27:40 +0000

    Don't verify the certificate
    git-svn-id: http://svn.rubyhaus.org/jira4r/trunk@14 eca90e12-3019-0410-aa8e-b962190924c8

---
+++ examples/getGroup-https.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#
+#If running as part of distribution
+require File.dirname(__FILE__) + '/../lib/jira4r/jira4r.rb'
+#If using GEM install
+#require 'jira4r/jira4r'
+
+#This is for testing the SSL connection; you will need to install http-access2 to really see the SSL 
+#certificate verification in action.
+
+jira = Jira::JiraTool.new(2, "https://jira.atlassian.com")
+
+jira.driver.options["protocol.http.ssl_config.verify_mode"] = nil
+
+jira.login("soaptester", "soaptester")
+
+jira.getGroups("montage-developers")
